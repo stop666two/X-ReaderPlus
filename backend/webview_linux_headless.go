8eb1986908c11f2ac8e4e86a1c3abceea50fc8eb
“stop666two” <stop666bilibili@gmail.com>
2026-06-28 06:09:06 +0800

    0.3.0版本紧急系统修复x2

---
+++ backend/webview_linux_headless.go
@@ -0,0 +1,18 @@
+//go:build linux && !cgo
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func runWebview(port string, _ *http.Server) {
+	log.Printf("Headless mode: open http://127.0.0.1:%s in your browser", port)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+}
