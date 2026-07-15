78f1f3ef5739675ebd6a2094faa4441766a0c22d
stop666two <stop666bilibili@gmail.com>
2026-07-15 21:38:56 +0800

    fix: run-server.bat 自动创建 frontend/.gitkeep 解决 embed 错误

--- backend/run-server.bat
+++ backend/run-server.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
 title X-ReaderPlus Backend
+if not exist frontend\ mkdir frontend
+if not exist frontend\.gitkeep echo . > frontend\.gitkeep
 set XREADER_SERVER_ONLY=true
 go run -tags desktop,dev .
 pause
